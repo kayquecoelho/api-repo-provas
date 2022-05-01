6e80aec6ae1a518162b16ff743da8255d21b6202
Kayque <lkcoelho76@gmail.com>
2022-05-01 16:15:39 -0300

    feat: implement test to route POST/tests

--- tests/authFactory/userFactory.ts
+++ tests/authFactory/userFactory.ts
@@ -1,9 +1,13 @@
 import { User } from "@prisma/client";
 import { prisma } from "../../src/connection.js";
+import * as bcryptService from "../../src/services/bcryptService.js";
 
-type CreateUserData = Omit<User, "id">
+type CreateUserData = Omit<User, "id">;
+
 export default async function userFactory(user: CreateUserData) {
+  const hashedPassword = bcryptService.encrypt(user.password);
+
   return prisma.user.create({
-    data: user,
+    data: {...user, password: hashedPassword}
   });
 }
--- tests/tests.test.ts
+++ tests/tests.test.ts
@@ -4,12 +4,14 @@ import { prisma } from "../src/connection.js";
 import dotenv from "dotenv";
 import userFactory from "./authFactory/userFactory.js";
 import userDataFactory from "./authFactory/userDataFactory.js";
+import { gunzip } from "zlib";
+import { TestData } from "../src/schemas/testSchema.js";
 dotenv.config();
 
 describe("POST /auth/sign-up", () => {
-  beforeEach(truncateUsersTable());
+  beforeEach(truncateUsersTable);
 
-  afterAll(disconnectDatabase());
+  afterAll(disconnectDatabase);
 
   it("should return status 422 given a empty body", async () => {
     const response = await supertest(app).post("/auth/sign-up");
@@ -46,9 +48,9 @@ describe("POST /auth/sign-up", () => {
 });
 
 describe("POST /auth/login", () => {
-  beforeEach(truncateUsersTable());
+  beforeEach(truncateUsersTable);
 
-  afterAll(disconnectDatabase());
+  afterAll(disconnectDatabase);
 
   it("should return 422 given a empty body", async () => {
     const response = await supertest(app).post("/auth/login");
@@ -80,14 +82,73 @@ describe("POST /auth/login", () => {
 });
 
 describe("POST /tests", () => {
-  it.todo("should return status 401 given a invalid token",);
+  beforeEach(truncateUsersTable);
 
-  it.todo("should return status 422 given an invalid body");
+  afterAll(disconnectDatabase);
 
-  it.todo("should return status 400 given invalid data");
+  it("should return status 401 given a invalid token", async () => {
+    const response = await supertest(app).post("/tests");
 
-  it.todo("should return status 201 given a valid body and persist test");
+    expect(response.status).toEqual(401);
+  });
+
+  it("should return status 422 given a valid token and an invalid body", async () => {
+    const token = await login();
+
+    const response = await supertest(app).post("/tests").set("Authorization", token);
+
+    expect(response.status).toEqual(422);
+  });
+  
+  it("should return status 400 given a valid body with invalid data", async () => {
+    const token = await login();
+    
+    const testData: TestData = {
+      name: "2022 - First Attempt",
+      pdfUrl: "http://www.google.com",
+      disciplineId: 25,
+      teacherId: 25,
+      categoryId: 15
+    }
+    
+    const response = await supertest(app).post("/tests").send(testData).set("Authorization", token);
+    
+    expect(response.status).toEqual(400);
+  });
+  
+  it("should return status 201 given a valid body and persist test", async () => {
+    const token = await login();
+    
+    const testData: TestData = {
+      name: "2022 - First Attempt",
+      pdfUrl: "http://www.google.com",
+      disciplineId: 2,
+      teacherId: 2,
+      categoryId: 1
+    }
+
+    const response = await supertest(app).post("/tests").send(testData).set("Authorization", token);
+    
+    const test = await prisma.test.findFirst({
+      where: {
+        name: "2022 - First Attempt"
+      }
+    });
+
+    expect(response.status).toEqual(201);
+    expect(test).not.toBe(null);
+  });
 });
+
+async function login(){
+  const user = userDataFactory();
+  
+  await userFactory(user);
+
+  const response = await supertest(app).post("/auth/login").send(user);
+
+  return response.body.token;
+}
 
 async function truncateUsersTable() {
   return await prisma.$executeRaw`TRUNCATE TABLE users CASCADE;`;
